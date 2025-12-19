ab10891ea74445ffac2291d6954672bf4c4e43bd
ericshuhari <ericdrichmond@gmail.com>
2025-12-18 23:40:06 -0500

    ssh client and server in working state

--- basic_networking/ssh_rcmd.py
+++ basic_networking/ssh_rcmd.py
@@ -13,14 +13,16 @@ def ssh_command(ip, port, user, passwd, cmd):
         ssh_session.send(cmd)
         print(ssh_session.recv(1024).decode())
         while True:
+            #continue to read commands from the command line
             cmd = ssh_session.recv(1024)
             try:
                 cmd_dc = cmd.decode()
                 if cmd_dc.lower() == 'exit':
                     client.close()
                     break
-                else:
-                    cmd_output = subprocess.check_output(shlex.split(cmd_dc), shell=True)
+                    #execute command
+                cmd_output = subprocess.check_output(shlex.split(cmd_dc), shell=True)
+                #send back the results to caller
                 ssh_session.send(cmd_output or 'okay')
             except Exception as e:
                 ssh_session.send(str(e))
@@ -29,9 +31,11 @@ def ssh_command(ip, port, user, passwd, cmd):
 
 if __name__ == '__main__':
     import getpass
-    user = getpass.getuser
+    # user = getpass.getuser()
+    user = input('Username: ')
     password = getpass.getpass()
 
     ip = input('Enter server IP: ')
     port = input('Enter port: ')
+    #invoke ssh_command function, send ClientConnected command
     ssh_command(ip, port, user, password, 'ClientConnected')
